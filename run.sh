489b9bdf2035b43146d2b9acffb6f90cc4678e73
Kevin Jones <kjones@Kevins-MacBook-Pro.local>
2015-08-18 12:34:58 -0700

    shell script update

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-docker run -d \
-  --name grafana \
-  --restart=always \
-  -p 3000:3000 \
-  #-v /var/lib/grafana:/var/lib/grafana \
-  #-v /etc/grafana:/etc/grafana \
-  #-e "GF_SECURITY_ADMIN_PASSWORD=p@55w0rd123" \
-  grafana/grafana:develop
+docker run -d -p 3000:3000 \
+  -v /var/lib/grafana:/var/lib/grafana \
+  --name grafana --restart=always grafana/grafana
